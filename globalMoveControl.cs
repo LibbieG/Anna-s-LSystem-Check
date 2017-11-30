786bdf1b0704e5d6effb3e92865eed974c92fab7
LibbieG <annagrossman@alum.calarts.edu>
2017-11-30 13:09:04 -0800

    Move Controls

---
+++ globalMoveControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalMoveControl : MonoBehaviour {
+
+	public static lsysLibrary [] lsysLibInst;
+	public static stringLSystem [] strlsysLibInst;
+	public int maxMovers = 25;
+	public static int lsysLibI;
+
+
+	// Use this for initialization
+	void Start () {
+		lsysLibInst = new lsysLibrary[maxMovers];
+		strlsysLibInst = new stringLSystem[maxMovers];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ meteorMoveControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorMoveControl : MonoBehaviour {
+	void Start (){
+		gameObject.GetComponent<stringLSystem>().enabled = false;
+		gameObject.GetComponent<lsysLibrary>().enabled = false;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.A)){
+			gameObject.GetComponent<stringLSystem>().enabled = true;
+			gameObject.GetComponent<lsysLibrary>().enabled = true;
+
+
+		} 
+		if (Input.GetKeyUp (KeyCode.D)) {
+			gameObject.GetComponent<stringLSystem>().enabled = false;
+			gameObject.GetComponent<lsysLibrary>().enabled = false;
+		}
+
+	}
+}
